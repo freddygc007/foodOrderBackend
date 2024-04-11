bf42608ada582acf0c2c8caa8c4a3e747a653e17
freddygc007 <freddygc007@gmail.com>
2024-04-12 01:04:45 +0530

    database connected

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,20 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
 import "dotenv/config";
+import mongoose from "mongoose";
+
+mongoose
+  .connect(process.env.MONGODB_CONNECTION_STRING as string)
+  .then(() => console.log("connected to db"));
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get("/test", async (req:Request, res:Response)=>{
-    res.json({message:"Hello !"})
+app.get("/test", async (req: Request, res: Response) => {
+  res.json({ message: "Hello !" });
 });
 
-app.listen(7000, ()=>{
-    console.log("server starts at the port 7000");
-    
-})
+app.listen(7000, () => {
+  console.log("server starts at the port 7000");
+});
